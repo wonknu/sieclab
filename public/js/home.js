ede82a72b09197884f24a436b814edbff3f5500f
guillaumedeplanque <Marcelino@MacBook-Pro-de-Guillaume.local>
2014-06-19 10:02:38 +0200

    Iphone animation

--- public/js/home.js
+++ public/js/home.js
@@ -1,9 +1,8 @@
 $( document ).ready(function() {
 
-
 	var socket = io.connect('http://192.168.1.9:3000');
 	socket.on('receive_shop', function (data) {
-	    console.log('animation');
+	    $('#list-brands-page').addClass('move');
 	});
 	$('.btn-data').on('touchend', function() {
 	    socket.emit('btn-data');
@@ -41,23 +40,5 @@ $( document ).ready(function() {
   		}, 1000);
   	})
   	});
-
-  	/*$(".brands-promo-img").on('touchend', function() {
-  		$(".promo-text").fadeOut(2000, function() {
-  			$(".check-in-no").css('display', 'block');
-  		});
-  		setTimeout(function() {
-  			$(".check-in-no").on('touchend', function() {
-  				$('.check-in-no').attr('src', '/img/check-in-yes.png');
-  				$('.check-in-no').fadeOut(3000, function() {
-  					$('.final-promo-img').css('display', 'block');
-  					$('.clock-icon').css('display', 'block');
-  					$('.final-promo-text').css('display', 'inline-block');
-  					$('.brands-promo-img').addClass('complete');
-  					$('.check-in-text').addClass('complete');
-  				});	
-  			})
-  		}, 1000);
-  	})*/
 });
 
